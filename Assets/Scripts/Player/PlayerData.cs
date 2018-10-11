475dc95a0727b26248742af554be4660cc3a3360
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-10-11 20:59:09 +0530

    Added Player Movement Controller

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace FortBlast.Player
+{
+    public static class PlayerData
+    {
+        // Animator Params
+        public const string PlayerHorizontal = "Move H";
+        public const string PlayerVertical = "Move V";
+        public const string PlayerDead = "Dead";
+        public const string PlayerShooting = "Shooting";
+
+
+        // Movement Params
+        public const string HorizontalAxis = "Horizontal";
+        public const string VerticalAxis = "Vertical";
+    }
+}
--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementController : MonoBehaviour {
+namespace FortBlast.Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Animator))]
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [Header("Movement")]
+        public float movementSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private Rigidbody _playerRB;
+        private Animator _playerAnimator;
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
+        {
+            _playerRB = GetComponent<Rigidbody>();
+            _playerAnimator = GetComponent<Animator>();
+        }
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update()
+        {
+            float moveX = Input.GetAxis(PlayerData.HorizontalAxis);
+            float moveZ = Input.GetAxis(PlayerData.VerticalAxis);
+
+
+            SetAnimator(moveX, moveZ);
+            MovePlayer(moveX, moveZ);
+        }
+
+        private void SetAnimator(float moveX, float moveZ)
+        {
+            _playerAnimator.SetFloat(PlayerData.PlayerHorizontal, moveX);
+            _playerAnimator.SetFloat(PlayerData.PlayerVertical, moveZ);
+        }
+
+        private void MovePlayer(float moveX, float moveZ)
+        {
+            Vector3 xVelocity = Vector3.zero;
+            Vector3 zVelocity = Vector3.zero;
+
+            if (moveZ != 0)
+                zVelocity = transform.forward * moveZ;
+
+            if (moveX != 0)
+                xVelocity = transform.right * moveX;
+
+            Vector3 combinedVelocity = (zVelocity + xVelocity) * movementSpeed * Time.deltaTime;
+            _playerRB.velocity = new Vector3(combinedVelocity.x, _playerRB.velocity.y, combinedVelocity.z);
+        }
+    }
 }
