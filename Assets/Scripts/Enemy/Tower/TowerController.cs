75355974d2cc2b67232f56141275f5851d0c5c8c
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-10-14 00:19:07 +0530

    Added Laser Shooting to Tower

--- Assets/Scripts/Enemy/Tower/TowerController.cs
+++ Assets/Scripts/Enemy/Tower/TowerController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using FortBlast.Extras;
 using UnityEngine;
 
@@ -13,9 +12,10 @@ namespace FortBlast.Enemy.Tower
         public float rotationSpeed;
 
         [Header("Attack")]
-        public float attackAngleDifferenceTolerance;
+        public float attackAngleTolerance;
         public float waitTimeBetweenAttack;
         public float attackDamage;
+        public float playerBaseOffset;
 
         [Header("Attack Stuff")]
         public Transform towerTop;
@@ -23,12 +23,18 @@ namespace FortBlast.Enemy.Tower
         public GameObject laser;
 
         private Transform _player;
+        private Renderer _laserRenderer;
+        private bool _attackingPlayer;
 
         /// <summary>
         /// Start is called on the frame when a script is enabled just before
         /// any of the Update methods is called the first time.
         /// </summary>
-        void Start() => _player = GameObject.FindGameObjectWithTag(TagManager.Player)?.transform;
+        void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag(TagManager.Player)?.transform;
+            _attackingPlayer = false;
+        }
 
         /// <summary>
         /// Update is called every frame, if the MonoBehaviour is enabled.
@@ -38,10 +44,10 @@ namespace FortBlast.Enemy.Tower
 
         }
 
-        private bool CheckPlayerInsideFOV()
+        private float CheckPlayerInsideFOV()
         {
             if (_player == null)
-                return false;
+                return -1;
 
             Vector3 modifiedPlayerPosition = new Vector3(_player.position.x, 0, _player.position.z);
             Vector3 modifiedTowerTopPosition =
@@ -52,9 +58,20 @@ namespace FortBlast.Enemy.Tower
             float normalizedAngle = ExtensionFunctions.To360Angle(angleToPlayer);
 
             if (normalizedAngle <= maxLookAngle)
-                return true;
+                return normalizedAngle;
             else
+                return -1;
+        }
+
+        private bool IsAngleWithinToleranceLevel(float normalizedAngle)
+        {
+            if (normalizedAngle < 0)
                 return false;
+
+            if (normalizedAngle <= attackAngleTolerance)
+                return true;
+
+            return false;
         }
 
         private void LookAtPlayer()
@@ -62,7 +79,37 @@ namespace FortBlast.Enemy.Tower
             if (_player == null)
                 return;
 
-			
+            Vector3 lookDirection = _player.position - towerTop.position;
+            lookDirection.y = 0;
+
+            if (lookDirection != Vector3.zero)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+                towerTop.rotation = Quaternion.Slerp(towerTop.rotation, lookRotation,
+                    rotationSpeed * Time.deltaTime);
+            }
+        }
+
+        private void TileLaserTexture()
+        {
+            float textureTilling = Mathf.Sin(Time.time) * 4f + 1;
+            _laserRenderer.material.mainTextureScale = new Vector2(textureTilling, 1);
+        }
+
+        private IEnumerator AttackPlayer()
+        {
+            Quaternion rotation = Quaternion.LookRotation(_player.position - laserShootingPoint.position);
+            GameObject laserInstance = Instantiate(laser, laserShootingPoint.position, rotation);
+
+            LineRenderer lineRenderer = laserInstance.GetComponentInChildren<LineRenderer>();
+            lineRenderer.SetPosition(0, laserShootingPoint.position);
+            lineRenderer.SetPosition(1, _player.position + Vector3.up * playerBaseOffset);
+
+            _laserRenderer = lineRenderer.GetComponent<Renderer>();
+            _attackingPlayer = true;
+
+            yield return new WaitForSeconds(waitTimeBetweenAttack);
+            _attackingPlayer = false;
         }
     }
 }
