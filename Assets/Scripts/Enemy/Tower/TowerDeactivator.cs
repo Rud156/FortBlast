c454a0416c2eb4f67bf4b27960c1a1795c781113
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-10-15 10:14:48 +0530

    Added Tower Switch

--- Assets/Scripts/Enemy/Tower/TowerDeactivator.cs
+++ Assets/Scripts/Enemy/Tower/TowerDeactivator.cs
@@ -6,16 +6,18 @@ namespace FortBlast.Enemy.Tower
 {
     public class TowerDeactivator : MonoBehaviour
     {
+        [Header("UI Display")]
         public GameObject uiPrompt;
         public Slider timerSlider;
-        public float maxDistanceFromPLayer;
-        public float maxInteractionTime;
+        public GameObject towerSwitchLight;
+
+        [Header("Activation Stats")]
         public TowerController towerController;
+        public float maxInteractionTime;
 
-        private Transform _player;
         private float _currentInteractionTime;
-
         private bool _playerNearby;
+        private bool _inPlayerFOV;
         private bool _towerDeactivated;
 
         /// <summary>
@@ -24,10 +26,41 @@ namespace FortBlast.Enemy.Tower
         /// </summary>
         void Start()
         {
-            _player = GameObject.FindGameObjectWithTag(TagManager.Player)?.transform;
             _playerNearby = false;
+            _inPlayerFOV = false;
             _towerDeactivated = false;
+            _currentInteractionTime = 0;
         }
+
+        /// <summary>
+        /// OnTriggerEnter is called when the Collider other enters the trigger.
+        /// </summary>
+        /// <param name="other">The other Collider involved in this collision.</param>
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(TagManager.Player))
+                _playerNearby = true;
+        }
+
+        /// <summary>
+        /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+        /// </summary>
+        /// <param name="other">The other Collider involved in this collision.</param>
+        void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(TagManager.Player))
+                _playerNearby = false;
+        }
+
+        /// <summary>
+        /// OnBecameVisible is called when the renderer became visible by any camera.
+        /// </summary>
+        void OnBecameVisible() => _inPlayerFOV = true;
+
+        /// <summary>
+        /// OnBecameInvisible is called when the renderer is no longer visible by any camera.
+        /// </summary>
+        void OnBecameInvisible() => _inPlayerFOV = false;
 
         /// <summary>
         /// Update is called every frame, if the MonoBehaviour is enabled.
@@ -37,28 +70,15 @@ namespace FortBlast.Enemy.Tower
             if (_towerDeactivated)
                 return;
 
-            CheckIfPlayerInRangeAndDisplayPrompt();
             DisplayPrompt();
 
             CheckPlayerInteraction();
             UpdateSliderAndDeactivateTower();
         }
 
-        private void CheckIfPlayerInRangeAndDisplayPrompt()
-        {
-            if (_player == null)
-                return;
-
-            float distanceToPlayer = Vector3.Distance(_player.position, transform.position);
-            if (distanceToPlayer <= maxDistanceFromPLayer && !_towerDeactivated)
-                _playerNearby = true;
-            else
-                _playerNearby = false;
-        }
-
         private void DisplayPrompt()
         {
-            if (_playerNearby && _currentInteractionTime == 0)
+            if (_playerNearby)
                 uiPrompt.SetActive(true);
             else
                 uiPrompt.SetActive(false);
@@ -66,7 +86,7 @@ namespace FortBlast.Enemy.Tower
 
         private void CheckPlayerInteraction()
         {
-            if (Input.GetKey(KeyCode.E) && _playerNearby)
+            if (Input.GetKey(KeyCode.E) && _playerNearby && _inPlayerFOV)
             {
                 _currentInteractionTime += Time.deltaTime;
                 timerSlider.gameObject.SetActive(true);
@@ -84,6 +104,11 @@ namespace FortBlast.Enemy.Tower
             if (_currentInteractionTime >= maxInteractionTime)
             {
                 _towerDeactivated = true;
+
+                timerSlider.gameObject.SetActive(false);
+                uiPrompt.SetActive(false);
+                towerSwitchLight.SetActive(false);
+
                 towerController.DeactivateTower();
             }
         }
