182fea370586384f46a9e3d035393808f03316ca
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-10-16 22:58:58 +0530

    Added Inital Inventory Items

--- Assets/Scripts/Resources/InventoryItem.cs
+++ Assets/Scripts/Resources/InventoryItem.cs
@@ -2,8 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "InventoryItem", menuName = "inventory/Item")]
-public class InventoryItem : ScriptableObject
+namespace FortBlast.Resources
 {
-	
+    [CreateAssetMenu(fileName = "InventoryItem", menuName = "Inventory/Item")]
+    public class InventoryItem : ScriptableObject
+    {
+        public enum ItemType
+        {
+            Spawnable,
+            UpgradeHelper,
+            Consumable
+        };
+
+        [Header("Basic Data")]
+        public string displayName;
+        [TextArea]
+        public string description;
+        public Texture2D image;
+
+        [Header("Item Details")]
+        public ItemType type;
+        public GameObject prefab;
+
+        [Header("Item Specifics")]
+        public float healthAmount;
+        public GameObject glassBreakingAudioPrefab;
+    }
 }
