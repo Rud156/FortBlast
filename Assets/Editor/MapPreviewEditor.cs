05950c322790fad28f545085da953ddc585fbdf3
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-11-13 19:33:19 +0530

    Added Editor Extensions

---
+++ Assets/Editor/MapPreviewEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using FortBlast.ProceduralTerrain.ProceduralTerrainCreators;
+
+namespace FortBlast.EditorExtensions
+{
+    [CustomEditor(typeof(MapPreview))]
+    public class MapPreviewEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            MapPreview mapPreview = (MapPreview)target;
+
+            if (DrawDefaultInspector())
+                if (mapPreview.autoUpdate)
+                    mapPreview.DrawMapInEditor();
+
+            if (GUILayout.Button("Generate"))
+                mapPreview.DrawMapInEditor();
+        }
+    }
+}
---
+++ Assets/Editor/UpdatableDataEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using FortBlast.ProceduralTerrain.Settings;
+
+namespace FortBlast.EditorExtensions
+{
+    [CustomEditor(typeof(UpdatebleData), true)]
+    public class UpdatableDataEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            UpdatebleData data = (UpdatebleData)target;
+
+            if (GUILayout.Button("Update"))
+            {
+                data.NotifyOnValuesUpdated();
+                EditorUtility.SetDirty(target);
+            }
+        }
+    }
+}
