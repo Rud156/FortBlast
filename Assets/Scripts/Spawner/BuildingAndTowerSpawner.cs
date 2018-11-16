8c5155bb552dcfdfac3c4c451e2fbc20a7249f66
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-11-16 19:28:02 +0530

    Adding Building and Tower

---
+++ Assets/Scripts/Spawner/BuildingAndTowerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FortBlast.Spawner
+{
+    public class BuildingAndTowerSpawner : MonoBehaviour
+    {
+        #region Singleton
+
+        public static BuildingAndTowerSpawner instance;
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake()
+        {
+            if (instance == null)
+                instance = this;
+
+            if (instance != this)
+                Destroy(gameObject);
+        }
+
+        #endregion Singleton
+
+        public void CreateTowersAndBuildings(Vector3 meshVertex, Transform parent)
+        {
+			
+        }
+    }
+}
