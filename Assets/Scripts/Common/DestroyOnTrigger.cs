308192d99bdaf5924f45608c8bf4ca15dceeab1a
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-11-22 18:43:06 +0530

    Fixed Tower Bomb Explosion

---
+++ Assets/Scripts/Common/DestroyOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FortBlast.Common
+{
+    public class DestroyOnTrigger : MonoBehaviour
+    {
+        public bool destoryOthersOnTrigger;
+
+        /// <summary>
+        /// OnTriggerEnter is called when the Collider other enters the trigger.
+        /// </summary>
+        /// <param name="other">The other Collider involved in this collision.</param>
+        void OnTriggerEnter(Collider other)
+        {
+            if (destoryOthersOnTrigger)
+                Destroy(other.gameObject);
+            else
+                Destroy(gameObject);
+        }
+    }
+}
