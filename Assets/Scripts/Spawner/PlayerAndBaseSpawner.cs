bf317160caab1bd53ca8b28cdba059c59141c26a
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-04 11:38:55 +0530

    Updated UI

--- Assets/Scripts/Spawner/PlayerAndBaseSpawner.cs
+++ Assets/Scripts/Spawner/PlayerAndBaseSpawner.cs
@@ -14,13 +14,19 @@ namespace FortBlast.Spawner
         /// Start is called on the frame when a script is enabled just before
         /// any of the Update methods is called the first time.
         /// </summary>
-        void Start() => Fader.instance.fadeStart += FadeStart;
+        void Start()
+        {
+            player.GetComponent<Rigidbody>().isKinematic = true;
+            Fader.instance.fadeStart += FadeStart;
+        }
 
         private void FadeStart()
         {
             GameObject playerBaseInstance = Instantiate(playerBase, Vector3.zero, Quaternion.identity);
             Vector3 playerSpawnPoint = playerBaseInstance.transform.GetChild(0).position;
+
             player.transform.position = playerSpawnPoint;
+            player.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 }
