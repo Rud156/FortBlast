664e103e4643dfb49e5c7ffd57fa3b5b45404376
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-17 12:40:37 +0530

    Added Continuous Damage

---
+++ Assets/Scripts/Common/ContinuousDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FortBlast.Common
+{
+    public class ContinuousDamage : MonoBehaviour
+    {
+        public float damagePerSecond;
+
+        private HealthSetter _healthSetter;
+
+        private void Update()
+        {
+            if(_healthSetter == null)
+                return;
+            
+            _healthSetter.ReduceHealth(damagePerSecond * Time.deltaTime);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            HealthSetter healthSetter = other.GetComponent<HealthSetter>();
+            if (healthSetter != null)
+                _healthSetter = healthSetter;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            HealthSetter healthSetter = other.GetComponent<HealthSetter>();
+            if (healthSetter != null)
+                _healthSetter = null;
+        }
+    }
+}
