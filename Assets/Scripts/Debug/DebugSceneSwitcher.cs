6b6eac01e64e5ae921691869ae59a9d0a7c87d20
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-03-10 12:37:38 +0530

    Added Debug Testing Scene

---
+++ Assets/Scripts/Debug/DebugSceneSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace FortBlast.CustomDebug
+{
+    [RequireComponent(typeof(Button))]
+    public class DebugSceneSwitcher : MonoBehaviour
+    {
+        public int sceneIndex;
+
+        private Button _sceneSwitcherButton;
+
+        private void Start()
+        {
+            _sceneSwitcherButton = GetComponent<Button>();
+            _sceneSwitcherButton.onClick.AddListener(HandleSceneSwitch);
+        }
+
+        private void HandleSceneSwitch()
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
